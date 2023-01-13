d62bc92b9c5f2f1672aac0afc44ded3126a6733a
Thiago Henrique da Costa <thiago.henrique@eletrosom.local>
2023-01-12 23:42:07 -0300

    inserindo metodos de atualizar, e pesquisar e o projeto compilado .jar

--- src/main/java/br/com/springboot/cursojdevtreinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/cursojdevtreinamento/controllers/GreetingsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +54,19 @@ public class GreetingsController {
     	
     }
     
+    @GetMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+    	
+    	if(usuario.getId() == null) {
+    		return new ResponseEntity<String>("Id nao informado", HttpStatus.OK);
+    	}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    }
+    
     
     @GetMapping(value = "listatodos")
     @ResponseBody /*retorna para o corpo da resposta*/
@@ -72,4 +87,35 @@ public class GreetingsController {
     	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     	
     }
+    
+    @DeleteMapping(value = "delete")
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam(name="iduser") Long iduser){
+    	
+    	usuarioRepository.deleteById(iduser);
+    	
+    	return new ResponseEntity<String>("Usuario deletado com sucesso", HttpStatus.OK);
+    }
+    
+    
+    @GetMapping(value ="buscarusuerid")
+    @ResponseBody
+    public ResponseEntity<Usuario> buscarusuerid(@RequestParam(name="iduser") Long iduser){
+    	
+    	Usuario usuario = usuarioRepository.findById(iduser).get();
+    	
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+    
+    @GetMapping(value="buscarpornome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarpornome(@RequestParam(name="name") String name){
+    	
+    	List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+    	
+    	return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+    	
+    }
+    
+    
 }
